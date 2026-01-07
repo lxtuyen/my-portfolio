318b7294b9ef222a5dbfb5e577e5a35b6b7a8cd0
lxtuyen <127100115+lxtuyen@users.noreply.github.com>
2026-01-07 22:01:10 +0700

    feat: add BE BASE URL

--- app/configs/axios.js
+++ app/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://portfolio-backend-mfb0.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
